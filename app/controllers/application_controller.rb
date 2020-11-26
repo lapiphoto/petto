aaa37c99e62d052653d45ae16695a1da55e8a1c2
EC2 Default User <ec2-user@ip-172-31-95-235.ec2.internal>
2020-11-26 05:44:59 +0000

    aa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
+  
+  before_action :set_search
+  def set_search
+    @search = Pet.ransack(params[:q]) #ransackメソッド推奨
+    @search_pets = @search.result.all
+  end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,7 @@
 class PetsController < ApplicationController
 
   def index
-    @pets = Pet.all
+    @searchpets = Pet.all
   end
 
   def show
